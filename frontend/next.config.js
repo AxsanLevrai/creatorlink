1719884ae4e28a94f428f099198af37e0f75bd28
AxsanLevrai <santosaxel3628@mail.com>
2026-03-16 00:10:17 +0100

    Fix: activer CSS et images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   experimental: { missingSuspenseWithCSRBailout: false },
-  images: {
-    remotePatterns: [
-      { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'https', hostname: '*.creatorlink.io' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-    ],
-  },
+  output: 'export',
+  images: { unoptimized: true },
 };
 module.exports = nextConfig;
